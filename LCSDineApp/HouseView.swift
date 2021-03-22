b954428fb31e55a1e26d0c5c01d6e20ffc5d06f8
yining wang <ywang@lcs.on.ca>
2021-03-22 12:53:53 -0400

    Basic layout of HouseView

---
+++ LCSDineApp/HouseView.swift
@@ -0,0 +1,29 @@
+//
+//  HouseView.swift
+//  LCSDineApp
+//
+//  Created by Yining Wang on 2021-03-22.
+//
+
+import SwiftUI
+
+struct HouseView: View {
+    private var houses:[String] = ["Ryder", "", ""]
+    
+    var body: some View {
+        VStack{
+            List{
+                ForEach(0..<houses.count){ num in
+                    Text("\(houses[num])")
+                }
+            }
+            .navigationBarTitle("Houses")
+        }
+    }
+}
+
+struct HouseView_Previews: PreviewProvider {
+    static var previews: some View {
+        HouseView()
+    }
+}
