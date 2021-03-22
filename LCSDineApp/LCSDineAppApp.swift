31e48ebd0432b1332b90157db948d55fe196ef53
yining wang <ywang@lcs.on.ca>
2021-03-22 12:46:45 -0400

    Initial Commit

---
+++ LCSDineApp/LCSDineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LCSDineAppApp.swift
+//  LCSDineApp
+//
+//  Created by Yining Wang on 2021-03-22.
+//
+
+import SwiftUI
+
+@main
+struct LCSDineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
